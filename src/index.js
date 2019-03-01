275aa2f2895effc6e46382eb85af40d03689db51
FoxHLawII <jfrojas251@gmail.com>
2019-03-01 13:52:39 -0500

    Server send client web

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('./config/enviroment');
 
 const bodyParser = require('body-parser');
+const path = require('path');
 const express = require('express');
 
 const controllers = require('./controllers');
@@ -10,6 +11,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static('web/ng-almundo-test/dist/ng-almundo-test'));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "../web/ng-almundo-test/dist/ng-almundo-test", "index.html"));
+});
 app.use('/hotels', controllers.hotelsController);
 
 app.listen(process.env.PORT, () => {
